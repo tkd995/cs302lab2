408e19cc89fc3ccc3c0121dc343e392f9458059b
Michael Felix Batchelor <mbatche1@tesla17.eecs.utk.edu>
2025-02-05 13:21:02 -0500

    Implement list.cpp functions

--- proj2/list.cpp
+++ proj2/list.cpp
@@ -2,14 +2,24 @@
 
 
 List::List() {
-
+	*head = NULL;
+	size = 0;
 
 }
 
 List::~List() {
-
+	while(head != NULL){
+		node n = head&;
+		head = head->next;
+		delete n;
+	}
 }
-
 void List::push_front(const std::string &s) {
- 
+	node *n = new node;
+	n->number = stoi(s);
+	n->string = s;
+	if(!head) n->next = NULL;
+	else n->next = head;
+	size++;
+	head = n;
 }
