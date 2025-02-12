c4d460ddd65358d710b6e007b82c600f01245c79
Michael Felix Batchelor <mbatche1@tesla17.eecs.utk.edu>
2025-02-12 16:52:12 -0500

    Finalized qsort implementation, quicksort TODO

--- proj2/qsort.cpp
+++ proj2/qsort.cpp
@@ -3,7 +3,7 @@
 #include "volsort.h"
 
 #include <cstdlib>
-#include <stdlib.h>
+#include <cstring>
 #include <vector>
 #include <iostream>
 
@@ -17,24 +17,31 @@ int qComparisonInt(const void * a, const void * b) {
 	return ia->number - ib->number; //  sorts in ascending order
 }
 
+int qComparisonString(const void * a, const void * b) {
+
+	const Node * ia = *(const Node **) a;
+	const Node * ib = *(const Node **) b;
+
+	return strcmp(ia->string.c_str(),ib->string.c_str());
+}
+
 void qsort_sort(List &l, bool numeric) {
 
 	Node* cur = l.head;
-	if(numeric){
-		vector<int> vec;
-		while(cur != NULL){
-			vec.push_back(stoi(cur->string));
-			cur = cur->next;
-		}
-		qsort(&vec[0], vec.size(),sizeof(int),qComparisonInt );
+	vector<Node*> vec;
+	while(cur != NULL){
+		vec.push_back(cur);
+		cur = cur->next;
 	}
-	else {
-		vector<string> vec;
-		while(cur != NULL){
-			vec.push_back(cur->string);
-			cur = cur->next;
-		}
-		qsort(&vec[0], vec.size(),sizeof(string),qComparisonInt);
-	}
-}
+	if(numeric)
+		qsort(&vec[0], vec.size(),sizeof(Node*),qComparisonInt);
+	else
+		qsort(&vec[0], vec.size(),sizeof(Node*),qComparisonString);
 
+	for(size_t i = 0; i < vec.size(); i++){
+		vec[i]->next = vec[i+1];
+		if(i == vec.size() - 1)
+			vec[i]->next = NULL;
+	}
+	l.head = vec[0];
+}
