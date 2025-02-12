7ca2042a7f9a06ba2076e9ddf324b649c9fa74b4
Michael Felix Batchelor <mbatche1@tesla17.eecs.utk.edu>
2025-02-12 14:16:56 -0500

    Prototype qsort and quicksort implementation

--- proj2/qsort.cpp
+++ proj2/qsort.cpp
@@ -3,10 +3,38 @@
 #include "volsort.h"
 
 #include <cstdlib>
-#include <array>
+#include <stdlib.h>
+#include <vector>
 #include <iostream>
+
+using namespace std;
+
+int qComparisonInt(const void * a, const void * b) {
+
+	const Node * ia = *(const Node **) a;
+	const Node * ib = *(const Node **) b;
+
+	return ia->number - ib->number; //  sorts in ascending order
+}
 
 void qsort_sort(List &l, bool numeric) {
 
+	Node* cur = l.head;
+	if(numeric){
+		vector<int> vec;
+		while(cur != NULL){
+			vec.push_back(stoi(cur->string));
+			cur = cur->next;
+		}
+		qsort(&vec[0], vec.size(),sizeof(int),qComparisonInt );
+	}
+	else {
+		vector<string> vec;
+		while(cur != NULL){
+			vec.push_back(cur->string);
+			cur = cur->next;
+		}
+		qsort(&vec[0], vec.size(),sizeof(string),qComparisonInt);
+	}
 }
 
--- proj2/quick.cpp
+++ proj2/quick.cpp
@@ -13,14 +13,69 @@ Node *concatenate(Node *left, Node *right);
 // Implementations
 
 void quick_sort(List &l, bool numeric) {
+	qsort(l.head,numeric);
 }
 
-Node *qsort(Node *head, bool numeric) {
+Node *qsort(Node *head, bool numeric) {	
+	if(head == NULL || head->next == NULL)//Base Case: 0 or 1 element in the partition
+		return head;
+	Node* left;
+	Node* right;
+	Node* pivot = head;//pivot is first element
+	partition(head, pivot, left, right, numeric);
+	qsort(left, numeric); qsort(right,numeric);
+	concatenate(left,pivot);//re-add the pivot
+	concatenate(left,right);
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric) {
+	Node* leftTail = left;
+	Node* rightTail = right;
+	while(leftTail != NULL)
+		leftTail = leftTail->next;
+	while(rightTail != NULL)
+		rightTail = rightTail->next;
+
+	Node* cur = head->next;
+	if(numeric){
+		while(cur != NULL){
+			if(stoi(cur->string) < stoi(pivot->string)){	
+				*leftTail = *cur;
+				leftTail = leftTail->next;
+				leftTail = NULL;
+			}
+			else {
+				*rightTail = *cur;
+				rightTail = rightTail->next;
+				rightTail = NULL;
+			}
+			cur = cur->next;
+		}
+	}
+	else {//not numeric
+		while(cur != NULL){
+			if((cur->string).compare((pivot->string)) <= 0){	
+				*leftTail = *cur;
+				leftTail = leftTail->next;
+				leftTail = NULL;
+			}
+			else {
+				*rightTail = *cur;
+				rightTail = rightTail->next;
+				rightTail = NULL;
+			}
+			cur = cur->next;
+		}
+	}
 }
 
 Node *concatenate(Node *left, Node *right) {
+	if(left == NULL)//left partition is empty
+		return right;
+	Node* cur = left;
+	while(cur != NULL)//proceed to end of the left partition
+		cur = cur->next;
+	cur->next = right;//then connect it to the right partition
+	return left;
 }
 
