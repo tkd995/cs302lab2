2711beed16816e64189edd7ec49af99d8e7be752
tkd995 <tkd995@vols.utk.edu>
2025-02-12 23:33:17 -0500

    final working

--- proj2/quick.cpp
+++ proj2/quick.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <cstring>
 
+using namespace std;
+
 // Prototypes
 
 Node *qsort(Node *head, bool numeric);
@@ -18,61 +20,91 @@ void quick_sort(List &l, bool numeric) {
 }
 
 Node *qsort(Node *head, bool numeric) {	
+	cout<<"qsort"<<endl;
 	if(head == NULL || head->next == NULL)//Base Case: 0 or 1 element in the partition
 		return head;
 
-	Node* left;
-	Node* right;
+	Node* left = nullptr, *right = nullptr;
 	Node* pivot = head;//pivot is first element
-	partition(head, pivot, left, right, numeric);//divide
+	partition(pivot->next, pivot, left, right, numeric);//divide
 
-	qsort(left, numeric); qsort(right,numeric);
-	concatenate(left,pivot);//re-add the pivot
-	return concatenate(left,right);//conquer
+	left = qsort(left, numeric); 
+	right = qsort(right,numeric);
+
+	pivot->next = right;
+	return concatenate(left,pivot);//conquer
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric) {
-
-	Node* leftTail = left;
-	Node* rightTail = right;
+	cout<<"partition"<<endl;
+	
+	Node* leftTail = nullptr, *rightTail = nullptr;
+	/*
 	while(leftTail != NULL && leftTail->next != NULL)
 		leftTail = leftTail->next;
 	while(rightTail != NULL && leftTail->next != NULL)
 		rightTail = rightTail->next;
-
-	Node* cur = head->next;
+	cout<<"here"<<endl;*/
+	Node* cur = head;
 	while(cur != NULL){
 		Node* next = cur->next;
 		if(numeric){
-			if(cur->number < pivot->number){	
-				leftTail = cur;
-				leftTail->next = NULL;
+			if(cur->number < pivot->number){
+				if(left == nullptr) {
+					left = cur;
+					leftTail = left;
+				}
+				else{
+					leftTail->next = cur;
+					leftTail = leftTail->next;
+				}
 			}
 			else {
-				rightTail = cur;
-				rightTail->next = NULL;
+				if(right == nullptr){
+					right = cur;
+					rightTail = right;
+				}
+				else{
+					rightTail->next = cur;
+					rightTail = rightTail->next;
+				}
 			}
 		}
 		else { //if not numeric
-			if(strcmp(cur->string.c_str(), pivot->string.c_str())){	
-				leftTail = cur;
-				leftTail->next = NULL;
+			if((cur->string).compare(pivot->string) < 0){	
+				if(left == nullptr){
+					left = cur;
+					leftTail = left;
+				}
+				else{
+					leftTail->next = cur;
+					leftTail = leftTail->next;
+				}
 			}
 			else {
-				rightTail = cur;
-				rightTail->next = NULL;
+				if(right == nullptr) {
+					right = cur;
+					rightTail = right;
+				}
+				else{
+					rightTail->next = cur;
+					rightTail = rightTail->next;
+				}
 			}
 		}
 		cur = next;
 	}
+	if (leftTail != nullptr) leftTail->next = nullptr;
+	if (rightTail != nullptr) rightTail->next = nullptr;
 }
 Node *concatenate(Node *left, Node *right) {
-	if(left == NULL)//left partition is empty
-		return right;
+	cout<<"cat"<<endl;
+	if(left == NULL) return right;
+
 	Node* cur = left;
-	while(cur != NULL)//proceed to end of the left partition
+	while(cur->next != NULL)//proceed to end of the left partition
 		cur = cur->next;
+
 	cur->next = right;//then connect it to the right partition
 	return left;
 }
-
