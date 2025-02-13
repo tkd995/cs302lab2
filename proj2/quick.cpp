c9eb06c60af6a22debaadfdebd923f1b6cd55759
Michael Felix Batchelor <mbatche1@tesla17.eecs.utk.edu>
2025-02-12 22:04:34 -0500

    Unfinished quicksort implementation TODO partition

--- proj2/quick.cpp
+++ proj2/quick.cpp
@@ -3,6 +3,7 @@
 #include "volsort.h"
 
 #include <iostream>
+#include <cstring>
 
 // Prototypes
 
@@ -13,62 +14,58 @@ Node *concatenate(Node *left, Node *right);
 // Implementations
 
 void quick_sort(List &l, bool numeric) {
-	qsort(l.head,numeric);
+	l.head = qsort(l.head,numeric);
 }
 
 Node *qsort(Node *head, bool numeric) {	
 	if(head == NULL || head->next == NULL)//Base Case: 0 or 1 element in the partition
 		return head;
+
 	Node* left;
 	Node* right;
 	Node* pivot = head;//pivot is first element
-	partition(head, pivot, left, right, numeric);
+	partition(head, pivot, left, right, numeric);//divide
+
 	qsort(left, numeric); qsort(right,numeric);
 	concatenate(left,pivot);//re-add the pivot
-	concatenate(left,right);
+	return concatenate(left,right);//conquer
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric) {
+
 	Node* leftTail = left;
 	Node* rightTail = right;
-	while(leftTail != NULL)
+	while(leftTail != NULL && leftTail->next != NULL)
 		leftTail = leftTail->next;
-	while(rightTail != NULL)
+	while(rightTail != NULL && leftTail->next != NULL)
 		rightTail = rightTail->next;
 
 	Node* cur = head->next;
-	if(numeric){
-		while(cur != NULL){
-			if(stoi(cur->string) < stoi(pivot->string)){	
-				*leftTail = *cur;
-				leftTail = leftTail->next;
-				leftTail = NULL;
+	while(cur != NULL){
+		Node* next = cur->next;
+		if(numeric){
+			if(cur->number < pivot->number){	
+				leftTail = cur;
+				leftTail->next = NULL;
 			}
 			else {
-				*rightTail = *cur;
-				rightTail = rightTail->next;
-				rightTail = NULL;
+				rightTail = cur;
+				rightTail->next = NULL;
 			}
-			cur = cur->next;
 		}
-	}
-	else {//not numeric
-		while(cur != NULL){
-			if((cur->string).compare((pivot->string)) <= 0){	
-				*leftTail = *cur;
-				leftTail = leftTail->next;
-				leftTail = NULL;
+		else { //if not numeric
+			if(strcmp(cur->string.c_str(), pivot->string.c_str())){	
+				leftTail = cur;
+				leftTail->next = NULL;
 			}
 			else {
-				*rightTail = *cur;
-				rightTail = rightTail->next;
-				rightTail = NULL;
+				rightTail = cur;
+				rightTail->next = NULL;
 			}
-			cur = cur->next;
 		}
+		cur = next;
 	}
 }
-
 Node *concatenate(Node *left, Node *right) {
 	if(left == NULL)//left partition is empty
 		return right;
